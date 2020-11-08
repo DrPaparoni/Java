079bb4f0da667c9e47e064b731a697617115d8a0
Eric Walter <ericwalter@my.cwi.edu>
2020-11-07 19:48:49 -0700

    Learned how to use strings

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,15 @@ public class Main {
         char studentFirstInitial = 'E';
         char studentLastInitial ='W';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "Eric";
+        String studentLastName = "Walter";
+
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
     }
 }
