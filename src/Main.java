06af666ab71a8705244f9a14b1d97d8814c42b07
Eric Walter <ericwalter@my.cwi.edu>
2020-11-28 19:42:10 -0700

    Finished course on learning java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        Triangle triangleA = new Triangle(15,8,15,8,17);
-        Triangle triangleB = new Triangle(3,2.598,3,3,3);
+        StudentProfile profileOne = new StudentProfile("Eric", "Walter", "Software Programming", 3.0, 2020);
+        StudentProfile profileTwo = new StudentProfile("Bob", "Barker", "Announcing", 4.0, 2024);
 
-        double triangleAArea = triangleA.findArea();
-        System.out.println(triangleAArea);
-
-        double triangleBArea = triangleB.findArea();
-        System.out.println(triangleBArea);
+        profileTwo.incrementExpectedGraduationYear();
+        System.out.println(profileTwo.expectedYearToGraduate);
     }
 }
