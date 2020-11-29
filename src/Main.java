ea212b31c771770cfd5088d376fff65aaf8f5c83
Eric Walter <ericwalter@my.cwi.edu>
2020-11-28 18:21:24 -0700

    added parameters and return types to functions

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void announceDeveloperTeaTime() {
-        System.out.println("Waiting for developer tea time...");
-        System.out.println("Type in any word and press Enter to start developer tea time");
-        Scanner input = new Scanner(System.in);
-        input.next();
-        System.out.println("It's developer tea time!");
+    public static double calculateTotalMealPrice(double listedMealPrice,
+                                               double tipRate,
+                                               double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        return result;
     }
     public static void main(String[] args) {
-        System.out.println("Welcome to your new job");
+        double groupTotalMealPrice = calculateTotalMealPrice(100,.2,.08);
+        System.out.println(groupTotalMealPrice);
 
-        announceDeveloperTeaTime();
-
-        System.out.println("Write code");
-        System.out.println("Review code");
-
-        announceDeveloperTeaTime();
-
-        System.out.println("Get promoted!");
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(individualMealPrice);
     }
 }
