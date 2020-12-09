6cede4e81e8610f7be50dcc5e3a1ad6eaf94162b
Eric Walter <ericwalter@my.cwi.edu>
2020-12-08 21:28:47 -0700

    Finalized calculator and updated comments for presentation

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,12 @@ public class Main {
         double totCredits=0;
         double gpa;   //Declaring Variables
 
-        Scanner console = new Scanner(System.in);  //Prepares system to take input
+        Scanner console = new Scanner(System.in);  //Prepares console for inputs
         System.out.println("Please enter number of credits for class 1 (numerical)");
         credit1 = console.nextDouble();  //Takes the user's input for the variable
         System.out.println("Please enter your grade for class 1 (Letter grade)");
         grade = console.next().toUpperCase(); //Next input is grade.
-        gradeValue = getGradeValue(grade, gradeValue);
+        gradeValue = getGradeValue(grade, gradeValue); //Passes grade and gradeValue into getGradeValue method
         totPtsClass1 = gradeValue * credit1;
 
         System.out.println("Please enter number of credits for class 2 (numerical)");
@@ -48,7 +48,7 @@ public class Main {
 
         totPts=totPtsClass1+totPtsClass2+totPtsClass3+totPtsClass4;
         totCredits=credit1+credit2+credit3+credit4;
-        gpa=totPts/totCredits; //
+        gpa=totPts/totCredits; //Calculates average gpa based on all 4 classes
 
         System.out.printf("Your GPA is: %.2f\n", + gpa); // Rounds final gpa to decimal
     }
